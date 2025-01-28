c08e1071852ee90cd7297bc9667acf7ce7a80fbb
Yap <microbious101@gmail.com>
2025-01-28 18:29:18 +1300

    removed models i dont need

--- next-server/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "standalone",
-  // https://nextjs.org/docs/app/api-reference/next-config-js/serverExternalPackages
-  serverExternalPackages: ["@huggingface/transformers"],
-};
-
-export default nextConfig;
